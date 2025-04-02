4c0ac9d174e1c2411a2d89ed83bc26b3aec44ee9
Paweł <rhsc.rsc@gmail.com>
2025-04-02 20:23:39 +0200

    Add files via upload

---
+++ vBot/npc_talk.lua
@@ -0,0 +1,9 @@
+onAttackingCreatureChange(function(creature, OldCreature)
+    if creature and creature:isNpc() and creature:getName() == "nn" and distanceFromPlayer(creature:getPosition()) <= 3 then
+        CaveBot.Conversation("hi", "trade")
+     elseif creature and creature:isNpc() and creature:getName() == "nn" and distanceFromPlayer(creature:getPosition()) <= 3 then
+        CaveBot.Conversation("hi", "trade")
+        elseif creature and creature:isNpc() and creature:getName() == "nn" and distanceFromPlayer(creature:getPosition()) <= 3 then
+        CaveBot.Conversation("hi", "trade")
+  end
+end)
