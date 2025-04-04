421c0850d2f78c32e2390f607a195a8e907af48e
Paweł <rhsc.rsc@gmail.com>
2025-04-04 04:01:20 +0200

    Update example_functions.lua

--- cavebot/example_functions.lua
+++ cavebot/example_functions.lua
@@ -5,18 +5,8 @@ local function addExampleFunction(title, text)
 end
 
 addExampleFunction("Click to browse example functions", [[
--- available functions/variables:
--- prev - result of previous action (true or false)
--- retries - number of retries of current function, goes up by one when you return "retry"
--- delay(number) - delays bot next action, value in milliseconds
--- gotoLabel(string) - goes to specific label, return true if label exists
--- you can easily access bot extensions, Depositer.run() instead of CaveBot.Extensions.Depositer.run()
--- also you can access bot global variables, like CaveBot, TargetBot
--- use storage variable to store date between calls
-
--- function should return false, true or "retry"
--- if "retry" is returned, function will be executed again in 20 ms (so better call delay before)
-
+CaveBot.Conversation("hi", "tak")
+delay(500)
 return true
 ]])
 
